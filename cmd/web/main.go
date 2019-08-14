014e8b1f7e36ad82193fe0633327b1ff775954c3
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-08-14 18:23:22 +0100

    Added a few more bits

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
@@ -14,10 +17,18 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "host=localhost  port=5432  user=adam-macbook  password=  dbname=postgres  sslmode=disable", "")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil { errorLog.Fatal(err)
+	}
+	// We also defer a call to db.Close(), so that the connection pool is closed // before the main() function exits.
+	defer db.Close()
+
 
 	app := &application{
 		errorLog:	errorLog,
@@ -31,6 +42,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting Server On %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err!= nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module contra-design.com/new_snippetbox
 
 go 1.12
+
+require (
+	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/lib/pq v1.2.0
+)
