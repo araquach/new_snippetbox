ab93f453b8a7ca1293e89bcef28d0fcce5d17fab
Adam Carter <adam@contra-design.com>
2019-08-24 17:03:36 +0100

    Added the new router - 178

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"github.com/bmizerany/pat"
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
+	mux := pat.New()
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.Get("/static/", http.StripPrefix("/static", fileserver))
 
-	return secureHeaders(mux)
+	return standardMiddleware.Then(mux)
 }
