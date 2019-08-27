18a6355e5af6b562162da653ecf69761dacdb019
Adam Carter <adam@contra-design.com>
2019-08-27 18:06:32 +0100

    Getting the validation errors to work
    and failing

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,12 +3,15 @@ package main
 import (
 	"contra-design.com/new_snippetbox/pkg/models"
 	"html/template"
+	"net/url"
 	"path/filepath"
 	"time"
 )
 
 type templateData struct {
 	CurrentYear int
+	FormData url.Values
+	FormErrors map[string]string
 	Snippet 	*models.Snippet
 	Snippets 	[]*models.Snippet
 }
