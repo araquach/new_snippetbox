937fe50a7c6fec997fc67a250b7fd9324414b240
Adam Carter <adamcarter@jakatasalon.co.uk>
2019-08-27 20:35:20 +0100

    Got the validation to work

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -23,8 +23,10 @@ func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
 
-func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData { if td == nil {
-	td = &templateData{} }
+func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	if td == nil {
+		td = &templateData{}
+	}
 	td.CurrentYear = time.Now().Year()
 	return td
 }
@@ -38,7 +40,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf := new(bytes.Buffer)
 
-	err := ts.Execute(buf, td)
+	err := ts.Execute(buf, app.addDefaultData(td, r))
 	if err != nil {
 		app.serverError(w, err)
 		return
